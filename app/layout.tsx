11fe19e816a65059ffaa2718b436a4196f4510af
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2024-12-29 20:56:58 +0900

    fix: CSSファイルのインポートパスを修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
-import "css/styles.css";
+import "css/style.css";
 import "css/L.Icon.Pulse.css";
 
 const geistSans = Geist({
