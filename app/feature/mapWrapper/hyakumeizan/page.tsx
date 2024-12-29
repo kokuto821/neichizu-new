af02df69add85c4ac0cee203f08c797ff7131900
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2024-12-29 23:37:01 +0900

    feat: MapComponentをmapWrapperに統合し、iframeを削除

--- app/feature/mapWrapper/hyakumeizan/page.tsx
+++ app/feature/mapWrapper/hyakumeizan/page.tsx
@@ -1,8 +1,9 @@
 // app/feature/mapWrapper/hyakumeizan.tsx
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { Header } from '@/app/components/molecules/header';
+import Link from "next/link";
+import Image from "next/image";
+import { Header } from "@/app/components/molecules/header";
+import MapComponent from "../../map/hyakumeizan/MapComponent";
 
 const Hyakumeizan = () => {
   return (
@@ -13,28 +14,33 @@ const Hyakumeizan = () => {
       {/* スマホナビの表示・非表示 */}
       <div className="map_title">
         <h2 className="h2-title_text">
-          <Image src="/img/mountain.png" width={30} height={30} alt="山のアイコン" />
+          <Image
+            src="/img/mountain.png"
+            width={30}
+            height={30}
+            alt="山のアイコン"
+          />
           日本百名山
         </h2>
       </div>
 
       <div className="map_wrap">
-        <iframe 
-          src="/feature/map/hyakumeizan" // Next.jsではPHPファイルではなく、ルートを指定
-          className="mapflame"
-          style={{ border: 'none', width: '100%', height: '500px' }} // iframeのスタイルを追加
-        ></iframe>
+        <MapComponent />
       </div>
 
       <footer className="foot_nav">
         <p className="foot_nav_p">
-          <Link href="https://www.google.com/maps/d/viewer?mid=1OjXFg_mjG2QeZTXK1enyj0957jiP9Og&ll=38.66483464924118%2C140.62674284694677&z=7" target="_blank" rel="noopener noreferrer">
+          <Link
+            href="https://www.google.com/maps/d/viewer?mid=1OjXFg_mjG2QeZTXK1enyj0957jiP9Og&ll=38.66483464924118%2C140.62674284694677&z=7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Googlemapで見る
           </Link>
         </p>
       </footer>
     </div>
   );
-}
+};
 
 export default Hyakumeizan; // デフォルトエクスポート
