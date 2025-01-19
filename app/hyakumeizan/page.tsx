de3176d8fac113396d19d6a1d1eb8896c6bf9007
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-01-19 23:10:37 +0900

    feat: Hyakumeizanページのパスを変更し、インポート文を修正

--- app/hyakumeizan/page.tsx
+++ app/hyakumeizan/page.tsx
@@ -1,8 +1,11 @@
 // app/feature/map/hyakumeizan/page.tsx
 "use client";
 
-import { initializeMap, DEFAULT_CONFIG } from "./utils/initializeMap";
-import { MapRenderer } from "./component/MapRenderer";
+import {
+  initializeMap,
+  DEFAULT_CONFIG,
+} from "../feature/map/hyakumeizan/utils/initializeMap";
+import { MapRenderer } from "../feature/map/hyakumeizan/component/MapRenderer";
 
 export default function HyakumeizanPage() {
   return (
