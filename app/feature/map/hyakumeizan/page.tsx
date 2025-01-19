25da0cb4be247bc6ebd29f8a040ff95297b33617
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-01-19 22:53:33 +0900

    feat: Hyakumeizanページをデフォルトエクスポートに変更し、"use client"を追加

--- app/feature/map/hyakumeizan/page.tsx
+++ app/feature/map/hyakumeizan/page.tsx
@@ -1,8 +1,13 @@
+// app/feature/map/hyakumeizan/page.tsx
+"use client";
+
 import { initializeMap, DEFAULT_CONFIG } from "./utils/initializeMap";
 import { MapRenderer } from "./component/MapRenderer";
 
-export const hyakumeizan: React.FC = () => (
-  <>
-    <MapRenderer initializeMap={initializeMap} config={DEFAULT_CONFIG} />
-  </>
-);
+export default function HyakumeizanPage() {
+  return (
+    <>
+      <MapRenderer initializeMap={initializeMap} config={DEFAULT_CONFIG} />
+    </>
+  );
+}
