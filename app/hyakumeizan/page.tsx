db9fe6337bc1017a606a043e173ede902e371a7a
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-02-03 22:58:32 +0900

    fix: フッターの背景色をEcruWhiteに変更し、テキストシャドウを削除。Googleマップリンクにクラス名を追加

--- app/hyakumeizan/page.tsx
+++ app/hyakumeizan/page.tsx
@@ -34,6 +34,7 @@ const Hyakumeizan = () => {
             href="https://www.google.com/maps/d/viewer?mid=1OjXFg_mjG2QeZTXK1enyj0957jiP9Og&ll=38.66483464924118%2C140.62674284694677&z=7"
             target="_blank"
             rel="noopener noreferrer"
+            className="foot_nav_link"
           >
             Googlemapで見る
           </Link>
