4b42306f583c026a9b6e939919a0b9f755846009
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-02-05 20:36:24 +0900

    refactor: ヘッダーのリンククラス名を変更し、スタイルを調整

--- app/components/molecules/header.tsx
+++ app/components/molecules/header.tsx
@@ -7,17 +7,17 @@ export const Header = () => {
     <div className="main_header">
       <h1 className="h1-title_text">
         <Image src="/img/logo.png" width={40} height={40} alt="Logo Icon" />
-        <Link href="/" className="a-title_text">
+        <Link href="/" className="main_header__title_text">
           ねいちず-NatureMaps
         </Link>
       </h1>
       <nav className="pc-nav">
         <ul>
           <li>
-            <Link href="/">TOP</Link>
+            <Link href="/" className="main_header__menu-text">TOP</Link>
           </li>
           <li>
-            <Link href="/other_list">OTHERS</Link>
+            <Link href="/other_list" className="main_header__menu-text">OTHERS</Link>
           </li>
         </ul>
       </nav>
