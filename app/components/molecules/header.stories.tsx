afe36b1f45d3ea42def5b62d148f97bda7192ade
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-02-11 20:05:49 +0900

    feat: headerコンポーネントのstoryを追加

---
+++ app/components/molecules/header.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { Header } from './header';
+
+
+export default {
+  title: 'Components/Header', // Storybookのサイドバーに表示されるタイトル
+  component: Header, // 対象のコンポーネント
+} as Meta;
+
+const Template: StoryFn = (args) => <Header {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  // ここにHeaderコンポーネントに渡すプロップスを定義できます
+  // 例えば、ロゴのパスやリンクのテキストを変更したい場合など
+};
