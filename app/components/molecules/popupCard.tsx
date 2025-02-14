24943468fe1d5f6118c99e25de3e1ff479f84dc2
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-02-14 22:42:37 +0900

    feat: 新しいPopupをフィーチャーごとに表示できるようにする

--- app/components/molecules/popupCard.tsx
+++ app/components/molecules/popupCard.tsx
@@ -1,40 +1,69 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { FeatureProperties } from '@/app/feature/map/hyakumeizan/types/types';
+import * as React from "react";
+import Image from "next/image";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { FeatureProperties } from "@/app/feature/map/hyakumeizan/types/types";
+import Link from "next/link";
 
 type Props = {
-  selectedFeature:FeatureProperties | null
-}
+  selectedFeature: FeatureProperties | null;
+};
 
-export const PopupCard = ({selectedFeature}:Props) => {
+export const PopupCard = ({ selectedFeature }: Props) => {
   return (
-    <Card sx={{ display: 'flex',position:"absolute" ,top:"0",left:"0", }}>
-          <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image={selectedFeature?.image} // 画像のURLを入れる
-        alt={selectedFeature?.name} // 山の名前を入れる
-      />
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Live From Space
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            component="div"
-            sx={{ color: 'text.secondary' }}
-          >
-            Mac Miller
-          </Typography>
-        </CardContent>
-
-      </Box>
-
-    </Card>
+    <div
+      className="popup-card"
+      style={{
+        position: "absolute",
+        bottom: "0",
+        left: "0",
+        transform: "translateY(-50%)",
+        width: "100%",
+      }}
+    >
+      <Card sx={{ display: "flex", alignItems: "center" }}>
+        <CardMedia
+          component="img"
+          sx={{ width: 151, height: 130 }}
+          image={selectedFeature?.image} // 画像のURLを入れる
+          alt={selectedFeature?.name} // 山の名前を入れる
+        />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography component="div" variant="h5">
+              {selectedFeature?.name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              component="div"
+              sx={{ color: "text.secondary" }}
+            >
+              {selectedFeature?.area} {selectedFeature?.height}
+            </Typography>
+            <Link href={selectedFeature?.googlemaplink || "#"} target="_blank">
+              <Image
+                className="link-img-logo"
+                src={"/img/g_map_logo.jpg"}
+                alt={"googleMap"}
+                width={110}
+                height={110}
+              />
+            </Link>
+            <Link href={selectedFeature?.YAMAP || "#"} target="_blank">
+              <Image
+                className="link-img-logo"
+                src={"/img/yamap-logo.png"}
+                alt={"Yamap"}
+                width={110}
+                height={110}
+              />
+            </Link>
+          </CardContent>
+        </Box>
+      </Card>
+    </div>
   );
-}
+};
