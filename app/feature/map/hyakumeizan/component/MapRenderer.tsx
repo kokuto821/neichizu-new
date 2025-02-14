3702134f4cf2beb340e90594db075483b0845769
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-02-14 22:47:28 +0900

    refactor: コードの整形とインポートパスの修正

--- app/feature/map/hyakumeizan/component/MapRenderer.tsx
+++ app/feature/map/hyakumeizan/component/MapRenderer.tsx
@@ -1,15 +1,19 @@
-import { useRef, useEffect, useState} from "react";
+import { useRef, useEffect, useState } from "react";
 import "ol/ol.css";
 import { FeatureProperties } from "../types/types";
 import Map from "ol/Map";
-import { useMapClick } from "../hook/useMapClick";
 import { initializeMap } from "../utils/initializeMap";
+import { useMapClick } from "../hooks/useMapClick";
 
 interface MapRendererProps {
-  setSelectedFeature: React.Dispatch<React.SetStateAction<FeatureProperties | null>>;
+  setSelectedFeature: React.Dispatch<
+    React.SetStateAction<FeatureProperties | null>
+  >;
 }
 
-export const MapRenderer: React.FC<MapRendererProps> = ({ setSelectedFeature }) => {
+export const MapRenderer: React.FC<MapRendererProps> = ({
+  setSelectedFeature,
+}) => {
   const mapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<Map | null>(null);
   const [center] = useState<[number, number]>([139, 35]); // 初期中心座標
@@ -29,9 +33,14 @@ export const MapRenderer: React.FC<MapRendererProps> = ({ setSelectedFeature }) 
     };
   }, [center]);
 
-  useMapClick(map,setSelectedFeature);
+  useMapClick(map, setSelectedFeature);
 
-  return <div ref={mapRef} style={{ width: "100%", height: "100%" ,position:"relative" }} />;
+  return (
+    <div
+      ref={mapRef}
+      style={{ width: "100%", height: "100%", position: "relative" }}
+    />
+  );
 };
 
 export default MapRenderer;
