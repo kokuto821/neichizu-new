a0d04e10fae417a6e3e65b523d75360b1a4bb944
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-02-15 15:23:48 +0900

    refactor: コードスタイルを統一し、シングルクォートに変更

--- app/components/molecules/mapToolbar.tsx
+++ app/components/molecules/mapToolbar.tsx
@@ -1,7 +1,7 @@
-import { IconButton, styled } from "@mui/material";
-import LayersRoundedIcon from "@mui/icons-material/LayersRounded";
-import { color } from "@/app/css/color";
-import { useChangeVisible } from "@/app/feature/map/hyakumeizan/hooks/useChangeVisible";
+import { IconButton, styled } from '@mui/material';
+import LayersRoundedIcon from '@mui/icons-material/LayersRounded';
+import { color } from '@/app/css/color';
+import { useChangeVisible } from '@/app/feature/map/hyakumeizan/hooks/useChangeVisible';
 
 type Props = {
   changeOSMLayer: () => void;
@@ -11,21 +11,21 @@ type Props = {
 export const MapToolbar = ({ changeOSMLayer, changeGSILayer }: Props) => {
   const { isVisible: isLayerVisible, changeVisible: changeLayerVisible } =
     useChangeVisible();
-  const IconButtonBox = styled("div")({
+  const IconButtonBox = styled('div')({
     backgroundColor: color.EcruWhite,
-    borderRadius: "20px",
-    width: "40px",
-    height: "40px",
+    borderRadius: '20px',
+    width: '40px',
+    height: '40px',
   });
-  const MapToolbarInner = styled("div")({
-    display: "flex",
-    flexDirection: "column",
+  const MapToolbarInner = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
   });
 
   return (
     <div
       className="map_toolbar"
-      style={{ position: "absolute", top: "0", left: "0", padding: "5px" }}
+      style={{ position: 'absolute', top: '0', left: '0', padding: '5px' }}
     >
       <MapToolbarInner>
         <IconButtonBox
@@ -33,17 +33,17 @@ export const MapToolbar = ({ changeOSMLayer, changeGSILayer }: Props) => {
             changeLayerVisible();
           }}
         >
-          <IconButton aria-label="layers" sx={{ color: "rgb(75, 139, 138)" }}>
+          <IconButton aria-label="layers" sx={{ color: 'rgb(75, 139, 138)' }}>
             <LayersRoundedIcon />
           </IconButton>
         </IconButtonBox>
-        {isLayerVisible ? null : (
+        {isLayerVisible ? (
           <>
             <IconButton onClick={() => changeGSILayer()}>gsi</IconButton>
             <br />
             <IconButton onClick={() => changeOSMLayer()}>osm</IconButton>
           </>
-        )}
+        ) : null}
       </MapToolbarInner>
     </div>
   );
