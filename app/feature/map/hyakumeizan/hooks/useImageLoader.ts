02891be46c428d01904f8447e13f8d0e3c7bae38
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-06-03 22:05:25 +0900

    fix: useImageLoaderのuseEffectから処理を分割

--- app/feature/map/hyakumeizan/hooks/useImageLoader.ts
+++ app/feature/map/hyakumeizan/hooks/useImageLoader.ts
@@ -1,4 +1,11 @@
-import { useState, useEffect, useRef, Dispatch, SetStateAction } from 'react';
+import {
+  useState,
+  useEffect,
+  useRef,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+} from 'react';
 import { FeatureProperties } from '../types/types';
 
 export const useImageLoader = (
@@ -8,35 +15,55 @@ export const useImageLoader = (
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   const imgRef = useRef<HTMLImageElement | null>(null);
 
-  useEffect(() => {
+  /**
+   * 画像をロードする関数
+   */
+  const loadImage = useCallback(() => {
     setIsImageLoaded(false);
 
-    if (selectedFeature?.image) {
-      const img = new Image();
-      imgRef.current = img;
-      img.src = selectedFeature.image;
-      img.onload = () => {
-        setIsImageLoaded(true);
-        setTimeout(() => {
-          setIsFeatureClick(false);
-        }, 700); // 1秒後にクリック状態をリセット
-      };
-      img.onerror = () => {
-        setIsImageLoaded(true); // エラー時も読み込み完了扱い
-        setIsFeatureClick(false);
-      };
-    } else {
-      setIsImageLoaded(true); // 画像がない場合は即時完了
-      setIsFeatureClick(false);
-    }
-
-    return () => {
+    // クリーンアップ
+    const cleanup = () => {
       if (imgRef.current) {
         imgRef.current.onload = null;
         imgRef.current.onerror = null;
       }
     };
-  }, [selectedFeature?.image, setIsFeatureClick]);
+
+    if (!selectedFeature) {
+      setIsImageLoaded(true);
+      setIsFeatureClick(false);
+      return cleanup;
+    }
+
+    if (selectedFeature.image) {
+      const img = new Image();
+      imgRef.current = img;
+      img.src = selectedFeature.image;
+
+      img.onload = () => {
+        setIsImageLoaded(true);
+        setTimeout(() => {
+          setIsFeatureClick(false);
+        }, 700);
+      };
+
+      img.onerror = () => {
+        setIsImageLoaded(true);
+        setIsFeatureClick(false);
+      };
+    } else {
+      setIsImageLoaded(true);
+      setIsFeatureClick(false);
+    }
+
+    return cleanup;
+  }, [selectedFeature, setIsFeatureClick]);
+
+  /** 初回レンダリング時と selectedFeature が変更されたときに loadImage を実行 */
+  useEffect(() => {
+    const cleanup = loadImage();
+    return cleanup;
+  }, [loadImage]);
 
   return { isImageLoaded };
 };
